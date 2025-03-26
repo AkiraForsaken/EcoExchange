d3aa88313ab1b2870bdcb5b584be7cc98f62bcaf
AkiraForsaken <dasdsa347@gmail.com>
2025-03-26 20:54:01 +0700

    Random commit for fun

--- ReactApp/src/components/Store.jsx
+++ ReactApp/src/components/Store.jsx
@@ -6,14 +6,14 @@ const Store = ({onClose}) => {
     <div className='modal-overlay'>
         <div className='store-container flex flex-wrap justify-around rounded-xl gap-5'>
           <span onClick={onClose} className="close-button right-5 text-xl">X</span>
-          <StoreCards price={15000}/>
-          <StoreCards price={15000}/>
-          <StoreCards price={15000}/>
-          <StoreCards price={15000}/>
-          <StoreCards price={15000}/>
-          <StoreCards price={15000}/>
-          <StoreCards price={15000}/>
-          <StoreCards price={15000}/>
+          <StoreCards price={10000}/>
+          <StoreCards price={10000}/>
+          <StoreCards price={10000}/>
+          <StoreCards price={10000}/>
+          <StoreCards price={10000}/>
+          <StoreCards price={10000}/>
+          <StoreCards price={10000}/>
+          <StoreCards price={10000}/>
         </div>
     </div>
   )
