1fbacbe34ae8b2050d84aba82650145f7e3fcbca
AkiraForsaken <dasdsa347@gmail.com>
2025-03-26 22:27:38 +0700

    Add some sample items

--- ReactApp/src/components/Store.jsx
+++ ReactApp/src/components/Store.jsx
@@ -6,14 +6,13 @@ const Store = ({onClose}) => {
     <div className='modal-overlay'>
         <div className='store-container flex flex-wrap justify-around rounded-xl gap-5'>
           <span onClick={onClose} className="close-button right-5 text-xl">X</span>
-          <StoreCards price={10000}/>
-          <StoreCards price={10000}/>
-          <StoreCards price={10000}/>
-          <StoreCards price={10000}/>
-          <StoreCards price={10000}/>
-          <StoreCards price={10000}/>
-          <StoreCards price={10000}/>
-          <StoreCards price={10000}/>
+          <h1 className='text-center text-4xl self-center w-full'>Exchange your points into items</h1>
+          <StoreCards type="toilet-paper" price={10000}/>
+          <StoreCards type="hao-hao" price={5000}/>
+          <StoreCards type="sting" price={13000}/>
+          <StoreCards type="toilet-paper" price={10000}/>
+          <StoreCards type="hao-hao"price={5000}/>
+          <StoreCards type="sting" price={13000}/>
         </div>
     </div>
   )
--- ReactApp/src/components/StoreCards.jsx
+++ ReactApp/src/components/StoreCards.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const StoreCards = ({price}) => {
+const StoreCards = ({type, price}) => {
+  const typeLabels = {
+    "toilet-paper":"Toilet paper",
+    "sting":"Sting energy drink",
+    "hao-hao":"Mì hảo hảo"
+  }
   return (
     <div className='flex-center flex-col bg-gray-100 w-[250px] h-[350px] p-3 rounded-xl'>
-        <img src="toilet-paper.jpg" alt="toilet-paper" />
-        <h3 className='text-2xl my-3'>Toilet paper</h3>
+        <img src={`${type}.jpg`} alt={type} />
+        <h3 className='text-xl my-3 text-center'>
+          {typeLabels[type] || "Item"} {/* Unknown is just generic "Item" */}
+        </h3>
         <button>Redeem: {price} points</button>
     </div>
   )
