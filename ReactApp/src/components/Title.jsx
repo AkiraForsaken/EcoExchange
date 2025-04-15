ebc960f2601232f0c618d0fcd3e6bc3856ca6852
AkiraForsaken <dasdsa347@gmail.com>
2025-04-15 14:44:54 +0700

    Blank profile pic

--- ReactApp/src/components/Title.jsx
+++ ReactApp/src/components/Title.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import logo from '../assets/logo.jpg'
+import profilePic from '../assets/profilepic.png'
 import { UserContext } from '../../context/UserContext'
 import toast from 'react-hot-toast'
 
@@ -8,7 +9,7 @@ const Title = ({ onLoginClick, onStoreClick, onUserInfoClick }) => {
     return (
         <nav className="flex justify-between items-center h-20 text-black text-2xl">
             <div className='flex-center p-4 gap-4'>
-                <img src={logo} alt='logo' className='h-15 w-15' />
+                <img src={logo} alt='logo' className='h-15 w-15 rounded-lg' />
                 <span className='text-4xl'>EcoExchange</span>
             </div>
             <div className='flex-center gap-8 p-4 '>
@@ -16,7 +17,11 @@ const Title = ({ onLoginClick, onStoreClick, onUserInfoClick }) => {
                 onClick={user ? onStoreClick : () => {toast.error('Not Logged In')}}> Store </a> : <></>}
                 <a className='cursor-pointer'> About </a>
                 <a className='cursor-pointer' 
-                onClick={user ? onUserInfoClick : onLoginClick}> {user ? user.name : "Log in"} </a>
+                onClick={user ? onUserInfoClick : onLoginClick}> 
+                    {user ? 
+                    <img src={profilePic} alt="profilepic" className='h-15 w-15 rounded-lg'/> 
+                    : "Log in"} 
+                </a>
             </div>
         </nav>
     )
